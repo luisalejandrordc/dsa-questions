8599f49b34ee1fbce7f09f0635808e889df2526f
Luis Alejandro <luisalejandrordc@gmail.com>
2026-05-04 17:18:51 -0500

    Update Data-Driven-Insights/14/14.cpp solution

--- Data-Driven-Insights/14/14.cpp
+++ Data-Driven-Insights/14/14.cpp
@@ -4,20 +4,18 @@
 
 using namespace std;
 
-unordered_map<char, char> openToClose = {{'(', ')'}, {'[', ']'}, {'{', '}'}};
 unordered_map<char, char> closeToOpen = {{')', '('}, {']', '['}, {'}', '{'}};
 
 bool validParentheses(string s) {
   // Last open bracket, must be the first to be closed (LIFO)
   stack<char> history;
   for (char c : s) {
-    if (openToClose.count(c))
-      history.push(c);
-    else {
+    if (closeToOpen.count(c)) {
       if (history.empty() || history.top() != closeToOpen[c])
         return false;
       history.pop();
-    }
+    } else
+      history.push(c);
   }
   return history.empty() ? true : false;
 }
@@ -26,8 +24,8 @@ int main() {
   cout << "It's Showtime Folks!" << endl;
   cout << "Result: " << validParentheses("()") << endl;
   cout << "Result: " << validParentheses("()[]{}") << endl;
-  cout << "Result: " << validParentheses("()[") << endl;
-  cout << "Result: " << validParentheses("()]") << endl;
-  cout << "Result: " << validParentheses("[(])") << endl;
+  cout << "Result: " << validParentheses("(]") << endl;
+  cout << "Result: " << validParentheses("([])") << endl;
+  cout << "Result: " << validParentheses("([)]") << endl;
   return 0;
 }
