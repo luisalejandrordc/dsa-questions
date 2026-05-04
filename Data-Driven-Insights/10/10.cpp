6dc4390a23eed1ed32c2b201290558f8fdd699c3
Luis Alejandro <luisalejandrordc@gmail.com>
2026-05-03 21:18:09 -0500

    Update Data-Driven-Insights/10/10.cpp solution

--- Data-Driven-Insights/10/10.cpp
+++ Data-Driven-Insights/10/10.cpp
@@ -6,39 +6,32 @@ using namespace std;
 
 const vector<pair<int, int>> DIRECTIONS = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
-bool dfs(vector<vector<char>> &grid, const string &word,
-         const pair<int, int> &size, pair<int, int> posi, int idx) {
+bool dfs(vector<vector<char>> &grid, const string &word, const int m,
+         const int n, int x, int y, int idx) {
   if (idx == word.length())
     return true;
-  for (pair<int, int> v : DIRECTIONS) {
-    int x = posi.first + v.first;
-    int y = posi.second + v.second;
-    if (x >= 0 && x < size.first && y >= 0 && y < size.second)
-      if (grid[x][y] == word[idx]) {
-        char temp = grid[x][y];
-        grid[x][y] = '\0';
-        int found = dfs(grid, word, size, {x, y}, idx + 1);
-        grid[x][y] = temp;
-        if (found)
-          return true;
-      }
-  }
+  if (x < 0 || x >= m || y < 0 || y >= n)
+    return false;
+  if (grid[x][y] != word[idx])
+    return false;
+  char temp = grid[x][y];
+  grid[x][y] = '\0';
+  for (const auto &v : DIRECTIONS)
+    if (dfs(grid, word, m, n, x + v.first, y + v.second, idx + 1)) {
+      grid[x][y] = temp;
+      return true;
+    }
+  grid[x][y] = temp;
   return false;
 }
 
 bool wordSearch(vector<vector<char>> &grid, string word) {
-  pair<int, int> size = {grid.size(), grid[0].size()};
-  for (int i = 0; i < size.first; i++)
-    for (int j = 0; j < size.second; j++) {
-      if (grid[i][j] == word[0]) {
-        char temp = grid[i][j];
-        grid[i][j] = '\0';
-        int found = dfs(grid, word, size, {i, j}, 1);
-        grid[i][j] = temp;
-        if (found)
-          return true;
-      }
-    }
+  int m = grid.size();
+  int n = grid[0].size();
+  for (int i = 0; i < m; i++)
+    for (int j = 0; j < n; j++)
+      if (dfs(grid, word, m, n, i, j, 0))
+        return true;
   return false;
 }
 
