befaebb2f4ca9ef5cf737c83afa51f1100060dd9
Luis Alejandro <luisalejandrordc@gmail.com>
2026-08-13 12:23:27 -0500

    add printVector function

--- IT-Hub/Arrays/2/2.cpp
+++ IT-Hub/Arrays/2/2.cpp
@@ -1,8 +1,15 @@
+#include "../../../include/utils.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+vector<int> brute_force(vector<int> &nums) { return nums; }
+
 int main() {
-  cout << "" << endl;
+  printTitle("Maximum Subarray");
+  vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+  cout << "Solution: ";
+  printVector(nums);
   return 0;
 }
--- include/utils.h
+++ include/utils.h
@@ -1,10 +1,18 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
 
 inline void printTitle(const std::string &title) {
   std::string border(title.length() + 10, '-');
   std::cout << border << std::endl;
   std::cout << "|    " << title << "    |" << std::endl;
   std::cout << border << std::endl;
+}
+
+inline void printVector(const std::vector<int> &nums) {
+  std::cout << "[\t";
+  for (const int x : nums)
+    std::cout << x << "\t";
+  std::cout << "]" << std::endl;
 }
