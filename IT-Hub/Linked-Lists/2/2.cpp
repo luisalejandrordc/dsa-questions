f507aa4a6f719278270aaf7347ddd12ebe6200cc
Luis Alejandro <luisalejandrordc@gmail.com>
2026-08-14 19:10:10 -0500

    add IT-Hub/Linked-Lists/2.cpp solution

--- IT-Hub/Linked-Lists/2/2.cpp
+++ IT-Hub/Linked-Lists/2/2.cpp
@@ -1,19 +1,36 @@
+#include "../../../include/linked_list.h"
 #include "../../../include/utils.h"
 #include <iostream>
+#include <unordered_set>
+#include <vector>
 
 using namespace std;
 
-struct ListNode {
-  int val;
-  ListNode *next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int val) : val(val), next(nullptr) {}
-  ListNode(int val, ListNode *next) : val(val), next(next) {}
-};
+bool hasCycle(ListNode *head) {
+  ListNode *curr = head;
+  unordered_set<ListNode *> nodes = {curr};
+  while (curr != nullptr) {
+    if (nodes.count(curr->next))
+      return true;
+    nodes.insert(curr);
+    curr = curr->next;
+  }
+  return false;
+}
 
 int main() {
   printTitle("Linked List Cycle");
-  ListNode *head;
+  vector<int> nums = {1, 2, 3, 4, 5};
+  ListNode *head = arrayToLinkedList(nums);
+  ListNode *curr = head;
+  while (true) {
+    if (curr->next == nullptr) {
+      curr->next = head;
+      break;
+    }
+    curr = curr->next;
+  }
+  // printLinkedList(head);
   cout << "Solution: " << hasCycle(head) << endl;
   return 0;
 }
