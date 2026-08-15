53892ddbc721fc3f81ac5057b2e06f4a388627ac
Luis Alejandro <luisalejandrordc@gmail.com>
2026-08-14 23:04:09 -0500

    add IT-Hub/Linked-Lists/3.cpp solution

---
+++ IT-Hub/Linked-Lists/3/3.cpp
@@ -0,0 +1,46 @@
+#include "../../../include/linked_list.h"
+#include "../../../include/utils.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+  ListNode *head = new ListNode();
+  ListNode *curr = head;
+  while (list1 != nullptr && list2 != nullptr) {
+    if (list1->val <= list2->val) {
+      curr->next = new ListNode(list1->val);
+      list1 = list1->next;
+    } else {
+      curr->next = new ListNode(list2->val);
+      list2 = list2->next;
+    }
+    curr = curr->next;
+  }
+  while (list1 != nullptr) {
+    curr->next = new ListNode(list1->val);
+    list1 = list1->next;
+    curr = curr->next;
+  }
+  while (list2 != nullptr) {
+    curr->next = new ListNode(list2->val);
+    list2 = list2->next;
+    curr = curr->next;
+  }
+  return head->next;
+}
+
+int main() {
+  printTitle("Merge Two Sorted Lists");
+  vector<int> nums1 = {1, 2, 4};
+  vector<int> nums2 = {1, 3, 4};
+  ListNode *list1 = arrayToLinkedList(nums1);
+  ListNode *list2 = arrayToLinkedList(nums2);
+  printLinkedList(list1);
+  printLinkedList(list2);
+  cout << "Solution: ";
+  ListNode *merged = mergeTwoLists(list1, list2);
+  printLinkedList(merged);
+  return 0;
+}
---
+++ IT-Hub/Linked-Lists/4/4_1.cpp
@@ -0,0 +1,29 @@
+#include "../../../include/linked_list.h"
+#include "../../../include/utils.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isPalindrome(ListNode *head) {
+  vector<int> nums;
+  while (head != nullptr) {
+    nums.push_back(head->val);
+    head = head->next;
+  }
+  int size = nums.size();
+  for (int l = 0, r = size - 1; l < r; l++, r--)
+    if (nums[l] != nums[r])
+      return false;
+  return true;
+}
+
+int main() {
+  printTitle("Palindrome Linked List");
+  vector<int> nums = {1, 2, 2, 1};
+  ListNode *head = arrayToLinkedList(nums);
+  printLinkedList(head);
+  string solution = isPalindrome(head) ? "True" : "False";
+  cout << "Solution: " << solution << endl;
+  return 0;
+}
