bc03d817f3b3692953c71ab368af7cd2b0e9fe58
Luis Alejandro <luisalejandrordc@gmail.com>
2026-08-16 01:55:49 -0500

    add IT-Hub/Stacks-Queues/2.cpp solution

--- IT-Hub/Stacks-Queues/2/2.cpp
+++ IT-Hub/Stacks-Queues/2/2.cpp
@@ -1,10 +1,52 @@
 #include "../../../include/utils.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+class MinStack {
+  vector<int> elements;
+  vector<int> minValue;
+
+public:
+  MinStack() {
+    elements = {};
+    minValue = {};
+  };
+
+  void push(int value) {
+    elements.push_back(value);
+    if (minValue.empty())
+      minValue.push_back(value);
+    else
+      minValue.push_back(min(minValue.back(), value));
+  }
+  void pop() {
+    if (!elements.empty()) {
+      elements.pop_back();
+      minValue.pop_back();
+    }
+  }
+  int top() { return elements.back(); }
+  int getMin() { return minValue.back(); }
+  void print() { printVector(elements); }
+};
+
 int main() {
   printTitle("Min Stack");
-  cout << "Solution:";
+  MinStack my_stack = MinStack();
+  my_stack.push(5);
+  my_stack.push(4);
+  my_stack.push(3);
+  my_stack.push(2);
+  my_stack.push(1);
+  my_stack.print();
+  cout << "Top value: " << my_stack.top() << endl;
+  cout << "Min value: " << my_stack.getMin() << endl;
+  my_stack.pop();
+  my_stack.pop();
+  my_stack.print();
+  cout << "Top value: " << my_stack.top() << endl;
+  cout << "Min value: " << my_stack.getMin() << endl;
   return 0;
 }
