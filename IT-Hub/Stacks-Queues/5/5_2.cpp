526d27ac8a232498e180f7c7f3e97b552b824bd2
Luis Alejandro <luisalejandrordc@gmail.com>
2026-08-17 18:48:59 -0500

    add IT-Hub/Stacks-Queues/5_2.cpp solution

---
+++ IT-Hub/Stacks-Queues/5/5_2.cpp
@@ -0,0 +1,49 @@
+#include "../../../include/utils.h"
+#include <deque>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findMaximum(queue<int> window) {
+  int maximum = window.front();
+  while (!window.empty()) {
+    maximum = max(maximum, window.front());
+    window.pop();
+  }
+  return maximum;
+}
+
+vector<int> maxSlidingWindow(vector<int> &nums, int k) {
+  queue<int> window;
+  vector<int> max_window;
+  int lastMax, removed;
+  for (int i = 0; i < k && i < nums.size(); i++)
+    window.push(nums[i]);
+  lastMax = findMaximum(window);
+  max_window.push_back(lastMax);
+  for (int i = k; i < nums.size(); i++) {
+    removed = window.front();
+    window.push(nums[i]);
+    window.pop();
+    if (nums[i] >= lastMax)
+      lastMax = nums[i];
+    else if (removed == lastMax)
+      lastMax = findMaximum(window);
+    max_window.push_back(lastMax);
+  }
+  return max_window;
+}
+
+int main() {
+  printTitle("Sliding Window Maximum");
+  // vector<int> nums = {1, 3, -1, -3, 5, 3, 6, 7};
+  vector<int> nums;
+  nums.reserve(100000);
+  for (int i = 10000; i >= 0; i--)
+    for (int j = 1; j <= 10; j++)
+      nums.push_back(i);
+  cout << "Solution: ";
+  printVector(maxSlidingWindow(nums, 50000));
+  return 0;
+}
---
+++ IT-Hub/Stacks-Queues/5/5_3.cpp
@@ -0,0 +1,39 @@
+#include "../../../include/utils.h"
+#include <deque>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> maxSlidingWindow(vector<int> &nums, int k) {
+  vector<int> maxWindow;
+  deque<int> maxs;
+  for (int i = 0; i < nums.size(); i++) {
+    // 1. Remove index that is outside the window
+    if (!maxs.empty() && maxs.front() == i - k)
+      maxs.pop_front();
+    // 2. Remove elements that are lower than the new element
+    while (!maxs.empty() && nums[maxs.back()] <= nums[i])
+      maxs.pop_back();
+    // 3. Push back new element to deque
+    maxs.push_back(i);
+    // 4. Push back max element to maxWindow
+    if (i >= k - 1)
+      maxWindow.push_back(nums[maxs.front()]);
+  }
+  return maxWindow;
+}
+
+int main() {
+  printTitle("Sliding Window Maximum");
+  // vector<int> nums = {1, 3, -1, -3, 5, 3, 6, 7};
+  vector<int> nums;
+  nums.reserve(100000);
+  for (int i = 10000; i >= 0; i--)
+    for (int j = 1; j <= 10; j++)
+      nums.push_back(i);
+  cout << "Finished ..." << endl;
+  cout << "Solution: ";
+  printVector(maxSlidingWindow(nums, 50000));
+  return 0;
+}
