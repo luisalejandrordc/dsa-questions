57cfa6d36291719856e336fc4fba1caaf256705e
Luis Alejandro <luisalejandrordc@gmail.com>
2026-08-18 14:12:06 -0500

    add PairHash struct in include/utils.h file

--- IT-Hub/Dynamic-Programming/3/3.cpp
+++ IT-Hub/Dynamic-Programming/3/3.cpp
@@ -3,38 +3,54 @@
 #include <climits>
 #include <functional>
 #include <iostream>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 
 using namespace std;
 
-int recs(vector<int> &coins, int amount, int idx) {
-  // You can make up the amount using only coins of denomination coins[idx]
-  if (amount % coins[idx] == 0) {
+int recs(vector<int> &coins, int idx, int amount,
+         unordered_map<pair<int, int>, int> &history) {
+  // You can make up the amount by using only coins[idx]
+  int quantity = amount / coins[idx];
+  int remaining = amount - quantity * coins[idx];
+  if (remaining == 0) {
     cout << "Coin: " << coins[idx] << "\t"
          << "Quantity: " << amount / coins[idx] << "\t END" << endl;
-    return amount / coins[idx];
+    return quantity;
   }
   // You need coins of different denominations, but already reached the end
   if (idx == coins.size() - 1)
     return -1;
   // Find a way to make up the amount using remaining denominations
   int result = INT_MAX;
-  for (int q = amount / coins[idx]; q >= 0; q--) {
-    int counter = recs(coins, amount - q * coins[idx], idx + 1);
-    if (counter != -1) {
-      result = min(result, counter + q);
-      cout << "Coin: " << coins[idx] << "\t" << "Quantity: " << q << endl;
-      // return counter + q;
+  while (quantity >= 0) {
+    pair<int, int> next = {idx + 1, remaining};
+    if (!history.count(next))
+      history[next] = recs(coins, next.first, next.second, history);
+    if (history.at(next) != -1) {
+      result = min(result, history.at(next) + quantity);
+      cout << "Coin: " << coins[idx] << "\t" << "Quantity: " << quantity
+           << endl;
     }
+    remaining -= coins[idx];
+    quantity--;
   }
   // Couldn't make up the the amount
   return result == INT_MAX ? -1 : result;
 }
 
+int coinChange(vector<int> &coins, int amount) {
+  sort(coins.begin(), coins.end(), greater<int>());
+  // key: <idx, amount> , value: <result>
+  unordered_map<pair<int, int>, int> history;
+  return recs(coins, 0, amount, history);
+}
+
 // This greedy solution has a problem: using as many of the largest coin
 // as possible does not mean you'll get the most optimal result (minimum)
 // Example: coins = {4, 3, 1}, amount = 6 -> Expected result = 2
-int recs2(vector<int> &coins, int amount, int idx) {
+int recs2(vector<int> &coins, int idx, int amount) {
   // You can make up the amount using only coins of denomination coins[idx]
   if (amount % coins[idx] == 0)
     return amount / coins[idx];
@@ -43,17 +59,12 @@ int recs2(vector<int> &coins, int amount, int idx) {
     return -1;
   // Find a way to make up the amount using remaining denominations
   for (int q = amount / coins[idx]; q >= 0; q--) {
-    int counter = recs(coins, amount - q * coins[idx], idx + 1);
+    int counter = recs2(coins, amount - q * coins[idx], idx + 1);
     if (counter != -1)
       return counter + q;
   }
   // Couldn't make up the the amount
   return -1;
-}
-
-int coinChange(vector<int> &coins, int amount) {
-  sort(coins.begin(), coins.end(), greater<int>());
-  return recs(coins, amount, 0);
 }
 
 int main() {
--- include/utils.h
+++ include/utils.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <chrono>
+#include <cstdint>
 #include <functional>
 #include <iostream>
 #include <string>
@@ -48,3 +49,12 @@ inline auto timedCall(Func &&func, Args &&...args) {
   std::cout << "Execution time: " << elapsed.count() << " us" << std::endl;
   return result;
 }
+
+template <typename T1, typename T2> struct PairHash {
+  std::size_t operator()(const std::pair<T1, T2> &p) const {
+    auto h1 = std::hash<T1>{}(p.first);
+    auto h2 = std::hash<T2>{}(p.second);
+    // Combine hashes (boost-style combine)
+    return h1 ^ (h2 + 0x9e3779b97f4a7c15ULL + (h1 << 6) + (h1 >> 2));
+  }
+};
