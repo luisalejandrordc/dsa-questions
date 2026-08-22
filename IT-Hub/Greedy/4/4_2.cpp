924d2b09d0e3cbbce9e73df8e4072a5844ac9fb4
Luis Alejandro <luisalejandrordc@gmail.com>
2026-08-22 09:48:40 -0500

    update IT-Hub/Greedy/4_2.cpp solution

--- IT-Hub/Greedy/4/4_2.cpp
+++ IT-Hub/Greedy/4/4_2.cpp
@@ -21,6 +21,33 @@ int eraseOverlapIntervals(vector<vector<int>> &intervals) {
   return erased;
 }
 
+// same algorithm, plus printing the final intervals
+int eraseOverlapIntervals1(vector<vector<int>> &intervals) {
+  sort(intervals.begin(), intervals.end(),
+       [](const vector<int> &a, const vector<int> &b) { return a[0] < b[0]; });
+
+  int erased = 0;
+  vector<int> lastInterval = intervals[0];
+  vector<vector<int>> finalIntervals;
+  for (int i = 1; i < intervals.size(); i++) {
+    if (intervals[i][0] < lastInterval[1]) {
+      if (intervals[i][1] < lastInterval[1])
+        lastInterval = intervals[i];
+      erased++;
+    } else {
+      finalIntervals.push_back(lastInterval);
+      lastInterval = intervals[i];
+    }
+    if (i == intervals.size() - 1)
+      finalIntervals.push_back(lastInterval);
+  }
+
+  for (const auto &v : finalIntervals)
+    printVector(v);
+
+  return erased;
+}
+
 int main() {
   printTitle("Non-overlapping Intervals");
   // vector<vector<int>> intervals = {{1, 2}, {2, 3}, {3, 4}, {1, 3}};
@@ -31,7 +58,7 @@ int main() {
       {-7, 97},   {-82, 9},   {-80, 3},  {87, 94}, {-96, 16}, {-15, 40},
       {-40, 86},  {31, 81},   {97, 98},  {69, 83}, {-40, 47}, {1, 82},
       {13, 44},   {-92, -65}, {51, 80}};
-  int solution = timedCall(eraseOverlapIntervals, intervals);
+  int solution = timedCall(eraseOverlapIntervals1, intervals);
   cout << "Solution: " << solution << endl;
   return 0;
 }
